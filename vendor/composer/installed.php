ee1f27525d5703da10a184ed64f005337eec2d8a
Simon Anastasov <simonanastasov2001@gmail.com>
2024-11-15 14:55:26 +0100

    Include compiled frontend assets and composer vendor folder for the Composer package version: 1.7.7-patch1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'digitalnodecom/woocommerce-brands',
-        'pretty_version' => '1.7.2',
-        'version' => '1.7.2.0',
+        'pretty_version' => '1.7.7',
+        'version' => '1.7.7.0',
         'reference' => null,
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'digitalnodecom/woocommerce-brands' => array(
-            'pretty_version' => '1.7.2',
-            'version' => '1.7.2.0',
+            'pretty_version' => '1.7.7',
+            'version' => '1.7.7.0',
             'reference' => null,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
